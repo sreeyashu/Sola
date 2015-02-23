66031ef413f73bb0c77818ee393513628c2b7ae3
sreeyashu <sreeyashu@gmail.com>
2015-02-23 12:28:12 +0100

    Yash

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
 	
-	$('.yash').popover({
+	$('.rasak').popover({
 	html:true,
 	content: function(){
 		return $(this).parent().find('.content').html();
 	},
-	container: '.yash',
+	container: '.rasak',
 	placement: 'bottom'
 	});
 	
